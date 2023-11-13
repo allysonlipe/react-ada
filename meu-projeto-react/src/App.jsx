8d29a6940038749ae461ac9861536f640b33cb1e
allysonlipe <allysongunner1@hotmail.com>
2023-11-13 01:19:40 -0300

    Componentes e props

--- meu-projeto-react/src/App.jsx
+++ meu-projeto-react/src/App.jsx
@@ -1,8 +1,20 @@
+import MeuComponente from "./components/MeuComponent"
+
 function App() {
 
   return (
-    <h1>Hello World!! - React</h1>
+    <div>
+      <h1>Hello World!! - React</h1>
+      <MeuComponente/>
+      <Botao conteudo='clique aqui' />
+      <Botao conteudo='depois aqui' />
+      <Botao conteudo='por fim, aqui' />
+    </div>
   )
+}
+
+function Botao(props) {
+  return(<button>{props.conteudo}</button>)
 }
 
 export default App
---
+++ meu-projeto-react/src/components/MeuComponent.jsx
@@ -0,0 +1,3 @@
+export default function MeuComponente() {
+    return (<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi beatae incidunt soluta, molestiae odit dignissimos quas adipisci eum laborum, ea distinctio repudiandae perferendis, temporibus minus iusto tenetur cum. Laudantium, dolorem?</p>)
+  }
